cfa21afded4dedd78d84a2d532e0063ea7026d9a
Jiazhou Chen <rgagechen@gmail.com>
2018-06-01 09:49:09 -0400

    new redcap 2 redcap transfer function (single subject loop)

--- R/MIGRATOR.R
+++ R/MIGRATOR.R
@@ -90,6 +90,110 @@ son.getideventmap<-function(ptc.from=NULL,data.from=NULL,...){
   return(f.id.map)
 }
 
+dnpl.redcap2redcap.ssub<-function(ptc.from=NULL,ptc.to=NULL,online=T,idmap=NULL,map=NULL,
+                                  trigger.a=NULL,trigger.b=NULL,
+                                  data.from=NULL,data.to=NULL,idvariable="record_id",
+                                  overwrite=F,bypass=F,functioncall=NULL,...) {
+  #This function is used for transfer data 
+  if (is.null(ptc.from) | is.null(ptc.to)) {stop("Need both protocol objects")}
+  if (is.null(map)) {stop("Mapping is missing")}
+  if (is.null(idmap)) {print("No ID mapping provided, will carry over IDs from 'a' to 'b' and transfer everyone's data")
+    #Need to generate ID Event Map Here:
+  }
+  if (online) {print("Online mode is on, will alway retrive from RedCap server...")}
+  if (is.null(data.from)) {
+    data.from<-bsrc.checkdatabase2(protocol = ptc.from,online = T)
+  }
+  if (is.null(data.to)) {
+    data.to<-bsrc.checkdatabase2(protocol = ptc.to,online = T)
+  }
+  #Generalizing function:
+  colnames(data.from$data)[grep(idvariable,names(data.from$data))]<-"record_id"
+  colnames(data.to$data)[grep(idvariable,names(data.to$data))]<-"record_id"
+  colnames(idmap)[grep(idvariable,names(idmap))]<-"record_id"
+  
+  #We will use person loops to ensure it always checks for completion before uploading
+  map->map.backup
+  
+  #for now, we will use single subject loop, not sure how efficient it would be but that's what's up:
+  envir.ept<-as.environment(emptyenv())
+  for (i in 1:length(idmap$record_id)) {
+    map<-map.backup
+    id<-idmap$record_id[i]
+    from.id<-idmap$idfield.from[i]
+    to.id<-idmap$idfield.to[i]
+    record.id.to<-data.to$data$record_id[match(idmap$idfield.to[which(idmap$record_id==id)],data.to$data$subject_id)]
+    print(paste("Processing from ID: ",from.id," to ID: ",to.id," now...",sep = ""))
+    
+    #Fix depends
+    if (any(map$from.event %in% "depends")) {
+      map$from.event[which(map$from.event %in% "depends")]<-idmap$redcap_event_name[idmap$record_id==id]
+    }
+    #Clean up personal map:
+    if (length(map$fixed)>0){
+      map<-map[which(map$from.event==idmap$redcap_event_name[idmap$record_id==id] | map$fixed),]
+    }
+    
+    #Checking status of completion and decide if to overwrite data
+    if (!overwrite) {
+      checkstatuslist<-paste(as.character(map$to.form),"_complete",sep = "")
+      tobeinvestigate<-data.to$data[c("record_id","redcap_event_name",checkstatuslist)]
+      tobeinvestigate<-tobeinvestigate[which(tobeinvestigate$redcap_event_name %in% unique(map$to.event)),]
+      tobeinvestigate[which(tobeinvestigate$record_id==record.id.to),]->singlesub
+      missingforms<-as.character(map$to.form[as.numeric(apply(singlesub[-c(1,2)],1,function(x) {which(x==0)
+      }))])} else {
+        missingforms<-map$from.form
+      }
+    map[which(missingforms %in% map$from.form),]->map
+    
+    #Step Verification:
+    #Even with just 0 length, the map will pass, allowing the loop to continue; 
+    if (!bypass){
+      if (!any(is.na(match(interaction(map$from.event,map$from.form),interaction(data.from$eventmap$unique_event_name,data.from$eventmap$form))))){
+        print("Passed form varification")
+      } else stop("Hey,some of these form names are not in the actual database, can't grab 'em if they ain't there!")
+      vari.from<-data.from$metadata$field_name[which(data.from$metadata$form_name %in% as.character(map$from.form))]
+      vari.to<-data.to$metadata$field_name[which(data.to$metadata$form_name %in% as.character(map$to.form))]
+      if (!any(is.na(match(vari.from,vari.to)))) {
+        print("Passed variable level varification")
+      } else {stop("Hey,some of the variables in from/to forms don't match, go fix it, use bypass=T or just use variable level match!")
+        print(vari.from[which(is.na(match(vari.from,vari.to)))])}
+    }
+    #Set up another loop to loop over to data event; i.e. only upload per person per event. 
+    if (length(map$from.form)>0) {
+      for (s.event in unique(map$to.event)) {
+        print(s.event)
+        map$from.form[which(map$to.event %in% s.event)]->from.form
+        map$from.event[which(map$to.event %in% s.event)]->rss.from.event
+        transfer.from.a<-bsrc.getform(curdb = data.from,res.event = rss.from.event,formname = from.form)
+        #transfer.from.a[which(transfer.from.a$redcap_event_name==s.event),]->transfer.from.b
+        transfer.from.a[which(transfer.from.a$record_id==id),]->transfer.from.c
+        transfer.from.c$record_id<-record.id.to
+        #Temporarily fixed, see brain for better solution:
+        transfer.from.c$redcap_event_name<-s.event
+        #Maybe do a check up before uploading everything. 
+        if (upload) {
+          #REDCapR::redcap_write(transfer.from.c,redcap_uri = ptc.to$redcap_uri,token = ptc.to$token)
+        } 
+      } #End Loop
+    }else {
+      print("Nothing to upload...next person...")
+    }
+  } 
+  
+  #Step Checking to make sure:
+  if (length()>0 | bypass) {
+    
+    
+  } else if (length()==0) {print("nothing to upload at all...wasting my time...argh...") 
+    pass<-false} else if (bypass) {"Bypassed checking procedure...risk of data contemedation is possible."}
+  
+  
+  
+  
+  
+}
+
 
 
 
