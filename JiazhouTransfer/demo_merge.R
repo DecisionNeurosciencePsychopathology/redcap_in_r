5feb4aa82979e636f497128decd676d588359ada
Jiazhou Chen <rgagechen@gmail.com>
2019-10-28 14:31:17 -0400

    update-jc

--- JiazhouTransfer/demo_merge.R
+++ JiazhouTransfer/demo_merge.R
@@ -1,6 +1,35 @@
 ####Jiazhou's mergeing script
 
-masterdemo <- bsrc.checkdatabase2(protocol = ptcs$masterdemo)
+masterdemo <- bsrc.getform(protocol = ptcs$masterdemo,formname = "record_registration",online = T,batch_size = 1000L)
 
 bs_demo<-bsrc.getform(protocol = ptcs$bsocial,formname = "record_registration",online = T,batch_size = 1000L)
+
+
+
+bs_demo_matched<-bs_demo[names(bs_demo)[names(bs_demo) %in% names(masterdemo)]]
+
+change_varinames<-list(terminate_yesno = "reg_term_yesno_bsocial",termination_date = "reg_termdate_bsocial",termination_reason = "reg_term_reason_bsocial",
+     termination_reason_other="reg_term_reason_n_bsocial",termination_reason_why="reg_term_reason_why",registration_id = "registration_wpicid",
+     registration_consentdate="reg_condate_bsocial")
+
+for (i in 1:length(change_varinames)){
+  bs_demo_matched[[change_varinames[[i]]]] <-  bs_demo[[names(change_varinames)[i]]]
+} 
+
+bs_demo_sp<-split(bs_demo_matched,paste0("is_",as.character(bs_demo_matched$registration_redcapid) %in% as.character(masterdemo$registration_redcapid)))
+
+
+df_new <- bs_demo_sp$is_TRUE
+df_rc_current <- masterdemo
+id = "registration_redcapid"
+id.new = NULL
+id.rc = NULL
+
+if(is.null(id.new) && is.null(id.rc))
+
+
+
+
+
+
 
--- kexin_codes/reference.R
+++ kexin_codes/reference.R
@@ -2,3 +2,13 @@
 choice_map<-bsrc.getchoicemapping(variablenames = "registration_gender",protocol = ptcs$masterdemo)
 
 df_new$registration_gender [!df_new$registration_gender %in% choice_map$choice.code]
+
+
+bsrc.verify<-function(df_new=NULL,df_ref=NULL,id.var=NULL){
+  if(any(!names(df_new) %in% names(df_ref))) {stop("New data frame has variables that is not in the RC one. Clean before input.")}
+  df_ref <- df_ref[names(df_new)] 
+  df_outcomp<-do.call(rbind,lapply(list(df_ref,df_new),reshape2::melt,id.var=c(id.var,"TYPE")))
+  df_comp<-reshape2::dcast(df_outcomp,formula =  as.formula(paste(paste(c(id.var,"variable"),collapse = "+"),"~ TYPE")),value.var = "value",fill = NA)
+  df_comp_sp<-split(df_comp,ifelse(df_comp$NEW==df_comp$REFERENCE,"SAME","DIFF"))
+  return(df_comp_sp)
+}
