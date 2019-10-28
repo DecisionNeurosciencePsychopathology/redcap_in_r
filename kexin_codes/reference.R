cc5a65eba5fc4a5daf102b1e975b796b7f9a0deb
Jiazhou Chen <rgagechen@gmail.com>
2019-10-28 14:38:08 -0400

    update the verify function to include a new func

--- kexin_codes/reference.R
+++ kexin_codes/reference.R
@@ -9,6 +9,12 @@ bsrc.verify<-function(df_new=NULL,df_ref=NULL,id.var=NULL){
   df_ref <- df_ref[names(df_new)] 
   df_outcomp<-do.call(rbind,lapply(list(df_ref,df_new),reshape2::melt,id.var=c(id.var,"TYPE")))
   df_comp<-reshape2::dcast(df_outcomp,formula =  as.formula(paste(paste(c(id.var,"variable"),collapse = "+"),"~ TYPE")),value.var = "value",fill = NA)
-  df_comp_sp<-split(df_comp,ifelse(df_comp$NEW==df_comp$REFERENCE,"SAME","DIFF"))
+  
+  is_both_na <- is.na(df_comp$NEW) & is.na(df_comp$REFERENCE)
+  is_same_value <- df_comp$NEW == df_comp$REFERENCE
+  is_same_value[is.na(is_same_value)] <- is_both_na[is.na(is_same_value)]
+  
+  df_comp_sp<-split(df_comp,ifelse(is_same_value,"SAME","DIFF"))
   return(df_comp_sp)
 }
+
