77fead9385f2d7b0ad39d389a07c3472e91ab850
Jiazhou Chen <rgagechen@gmail.com>
2020-01-28 13:16:40 -0500

    update

--- R/BASIC.R
+++ R/BASIC.R
@@ -1,33 +1,34 @@
 #Basics:
-redcap_uri=ptcs$protect$redcap_uri
-token=ptcs$protect$token
-
-action = NULL
-content = NULL
-arms = NULL
-records = NULL
-fields = NULL
-
-message = TRUE
-
-#' @param overwriteBehavior Character string.  'normal' prevents blank
-#' #'   fields from overwriting populated fields.  'overwrite' causes blanks to
-#' #'   overwrite data in the REDCap database.
-#' #' @param returnContent Character string.  'count' returns the number of
-#' #'   records imported; 'ids' returns the record ids that are imported;
-#' #'   'nothing' returns no message.
-#' #' @param returnData Logical.  Prevents the REDCap import and instead
-#' #'   returns the data frame that would have been given
-#' #'   for import.  This is sometimes helpful if the API import fails without
-#' #'   providing an informative message. The data frame can be written to a csv
-#' #'   and uploaded using the interactive tools to troubleshoot the
-#' #'   problem.  Please shoot me an e-mail if you find errors I havne't
-#' #'   accounted for.
+#' redcap_uri=ptcs$protect$redcap_uri
+#' token=ptcs$protect$token
+#' 
+#' action = NULL
+#' content = NULL
+#' arms = NULL
+#' records = NULL
+#' fields = NULL
+#' 
+#' message = TRUE
+#' 
+#' #' @param overwriteBehavior Character string.  'normal' prevents blank
+#' #' #'   fields from overwriting populated fields.  'overwrite' causes blanks to
+#' #' #'   overwrite data in the REDCap database.
+#' #' #' @param returnContent Character string.  'count' returns the number of
+#' #' #'   records imported; 'ids' returns the record ids that are imported;
+#' #' #'   'nothing' returns no message.
+#' #' #' @param returnData Logical.  Prevents the REDCap import and instead
+#' #' #'   returns the data frame that would have been given
+#' #' #'   for import.  This is sometimes helpful if the API import fails without
+#' #' #'   providing an informative message. The data frame can be written to a csv
+#' #' #'   and uploaded using the interactive tools to troubleshoot the
+#' #' #'   problem.  Please shoot me an e-mail if you find errors I havne't
+#' #' #'   accounted for.
 
 
 
 redcap_api_call<-function (redcap_uri=NULL, token=NULL,
-                           action = NULL, content = NULL,arms = NULL,records = NULL, fields = NULL,
+                           action = NULL, content = NULL,
+                           records = NULL,arms = NULL,events=NULL, forms=NULL, fields = NULL,
                            export_file_path = NULL,
                            message = TRUE,httr_config=NULL) {
   #Use this space to document
@@ -59,9 +60,11 @@ redcap_api_call<-function (redcap_uri=NULL, token=NULL,
     if (exists("ds") & inherits(ds, "data.frame")) {return(ds)} else {
       if(result$status != 200L) {message("redcap api call failed at converting stage, returning raw text");return(raw_text)}
     }
-  } else if (action == "export" || content == "pdf") {
-    
+  } else if (action == "export" || content %in% c("pdf")) {
+   stop("Function not yet available.")
   } else if (content == "records" && is.null(action)) {
+    
+  } else if (content == "records" && action == "delete"){
     
   }
   return(raw_text)
